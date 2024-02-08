b73a788a55fe6ba361dd3b221587d618e498d028
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-08 23:18:53 +0200

    lesson 8

---
+++ src/com/foretruff/http/socket/SocketRunner.java
@@ -0,0 +1,23 @@
+package com.foretruff.http.socket;
+
+import java.io.*;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class SocketRunner {
+    public static void main(String[] args) throws IOException {
+        //http - 80
+        //https -443
+        // tcp
+        var inetAddress = Inet4Address.getByName("google.com");
+        try (var socket = new Socket(inetAddress, 80);
+             var outputStream = new DataOutputStream(socket.getOutputStream());
+             var inputStream = new DataInputStream(socket.getInputStream())) {
+            outputStream.writeUTF("Hello world");
+            var response = inputStream.readAllBytes();
+            System.out.println(response.length);
+        }
+
+    }
+}
