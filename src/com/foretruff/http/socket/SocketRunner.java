ef225deead983fbbb306d0dc51f28f367ff78482
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-08 23:41:49 +0200

    lesson 9

--- src/com/foretruff/http/socket/SocketRunner.java
+++ src/com/foretruff/http/socket/SocketRunner.java
@@ -4,19 +4,25 @@ import java.io.*;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
     public static void main(String[] args) throws IOException {
         //http - 80
         //https -443
         // tcp
-        var inetAddress = Inet4Address.getByName("google.com");
-        try (var socket = new Socket(inetAddress, 80);
+        var inetAddress = Inet4Address.getByName("localhost");
+        try (var socket = new Socket(inetAddress, 8085);
              var outputStream = new DataOutputStream(socket.getOutputStream());
-             var inputStream = new DataInputStream(socket.getInputStream())) {
-            outputStream.writeUTF("Hello world");
-            var response = inputStream.readAllBytes();
-            System.out.println(response.length);
+             var inputStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)) {
+
+            while (scanner.hasNextLine()) {
+                var request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response from server: " + inputStream.readUTF());
+            }
+
         }
 
     }
