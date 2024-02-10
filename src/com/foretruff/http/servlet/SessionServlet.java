a4114e8600864f01c19719336d5ee17de81b65a5
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-11 00:19:14 +0200

    lesson 36

--- src/com/foretruff/http/servlet/SessionServlet.java
+++ src/com/foretruff/http/servlet/SessionServlet.java
@@ -15,7 +15,7 @@ public class SessionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var session = req.getSession();
-        session.
+        System.out.println(session.isNew());
     }
 
 }
