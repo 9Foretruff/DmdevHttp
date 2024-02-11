d1c9f683c0340e25503f435c24a230c58f0e8d89
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-11 23:16:35 +0200

    lesson 42

--- src/com/foretruff/http/servlet/ContentServlet.java
+++ src/com/foretruff/http/servlet/ContentServlet.java
@@ -1,5 +1,7 @@
 package com.foretruff.http.servlet;
 
+import com.foretruff.http.dto.FlightDto;
+import com.foretruff.http.service.FlightService;
 import com.foretruff.http.util.JspHelper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -8,11 +10,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 @WebServlet("/content")
 public class ContentServlet extends HttpServlet {
+    private final FlightService flightService = FlightService.getInstance();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var flightDtos = flightService.findAll();
+        req.setAttribute("flights", flightDtos);
+        req.getSession().setAttribute("flightsMap", flightDtos.stream()
+                .collect(toMap(FlightDto::getId,FlightDto::getDescription)));
         req.getRequestDispatcher(JspHelper.getPath("content")).forward(req,resp);
     }
 }
--- web/WEB-INF/jsp/content.jsp
+++ web/WEB-INF/jsp/content.jsp
@@ -8,7 +8,16 @@
 <%@include file="Header.jsp" %>
 
 <div>
-    Content привіт
+    <span>Content привіт</span>
+    <p>Size:${requestScope.flights.size()}</p>
+    <p>Id:${requestScope.flights.get(0).id}</p>
+    <p>Id 2:${requestScope.flights[1].id}</p>
+    <p>Map:${sessionScope.flightsMap[1]}</p>
+    <p>JSESSION:${cookie["JSESSIONID"].value}</p>
+    <p>Header:${header["Cookie"]}</p>
+    <p>Param id:${param.id}</p>
+    <p>Param test:${param.test}</p>
+    <p>Not Empty list:${not empty flights}</p>
 </div>
 
 <%@include file="footer.jsp" %>
