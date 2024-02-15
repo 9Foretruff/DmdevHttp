1afc38bb8517276e19d58a7ffd717d652ee4507d
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-02-15 18:49:08 +0200

    lesson 54

--- src/com/foretruff/http/locale/LocaleRunner.java
+++ src/com/foretruff/http/locale/LocaleRunner.java
@@ -1,16 +1,21 @@
 package com.foretruff.http.locale;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class LocaleRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
 //        Locale locale = new Locale("uk","UA");
-        Locale locale = new Locale.Builder().setLanguage("ru").setRegion("RU").build();
+        System.setProperty("file.encoding", "UTF-8");
+        Locale locale = new Locale.Builder().setLanguage("de").setRegion("LU").build();
         System.out.println(Locale.UK);
         System.out.println(Locale.getDefault());
 
-        var translations = ResourceBundle.getBundle("translations", locale); // ???????? ???? ?? ???????
-        System.out.println(translations.getString("page.login.password"));
+        var translations = ResourceBundle.getBundle("translation", locale);
+        var value = translations.getString("page.login.password");
+//        var string = new String(value.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+        System.out.println(value);
+
     }
 }
